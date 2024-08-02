16a0671648023a8a305c2cc663250a792ca055aa
bipblop555 <tbonnet@meylly.com>
2024-08-03 00:55:27 +0200

    ajout class player

---
+++ src/components/Player.ts
@@ -0,0 +1,32 @@
+class Plater {
+    private name:string;
+    private score:number;
+
+    private isDrawing: boolean;
+
+    constructor(name:string) {
+        this.name = name;
+        this.score = 0;
+        this.isDrawing = false;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getScore(): number {
+        return this.score;
+    }
+
+    public isDrawingPlayer(): boolean {
+        return this.isDrawing;
+    }
+
+    public setDrawingStatus(status:boolean): void {
+        this.isDrawing = status;
+    }
+
+    public incrementScore(points:number): void {
+        this.score += points;
+    }
+}
