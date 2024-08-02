5f8115c91a395c7c524c2eb7562862145a7e9a50
bipblop555 <tbonnet@meylly.com>
2024-08-03 01:51:47 +0200

    divers

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,42 @@ import './reset.css'
 import './app.css'
 
 const App: React.FC = () => {
+    const players = ["Top", "Michou"];
 
     useEffect(() => {
-        new DrawingApp();
+        new DrawingApp(players);
     }, []);
 
     return (
         <div className='wrapper'>
           <div className='scoring'>
-
+            <span id="currentPlayer"></span>
           </div>
           <div className='canvas_drawing_wrapper'>
+            <span id="currentWord"></span>
+
             <canvas id="canvas" width="1000" height="400" style={{ border: '1px solid black' }}></canvas>
-            <div className='drawing'>
+            <div className='drawing' id='palette'>
+              <div>
+              <button className='colors red'></button>
+              <button className='colors blue'></button>
+              <button className='colors yellow'></button>
+              <button className='colors red'></button>
+              </div>
+              <div>
+              <button className='colors red'></button>
+              <button className='colors red'></button>
+              <button className='colors red'></button>
+              <button className='colors red'></button>
+              </div>
               <button id="clear">Clear</button>
             </div>
           </div>
           <div className='chat'>
-
+            <form id="guess">
+              <input type="text" id="guessInput" placeholder="Enter your guess" />
+              <button id="guess" type="submit">Guess</button>
+            </form>
           </div>
         </div>
     );
