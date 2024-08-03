ef69027a869efcd0d034917e559b26b8702c59a7
bipblop555 <tbonnet@meylly.com>
2024-08-03 14:57:26 +0200

    modif db ajout tables

--- apps/api/docker/init.sql
+++ apps/api/docker/init.sql
@@ -1,45 +1,30 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE DATABASE ecomm;
-\c ecomm;
+CREATE DATABASE pictio;
+\c pictio;
 
-CREATE TABLE "categories" (
+CREATE TABLE "rooms" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(50)
-);
-
-CREATE TABLE "products" (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    categories_id UUID NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL,
-    quantity INTEGER NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    deleted_at TIMESTAMP,
-
-    CONSTRAINT fk_categories_products FOREIGN KEY (categories_id) REFERENCES categories(id) ON DELETE CASCADE
-);
-
-CREATE TABLE "basket" (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    content JSONB
-);
-
-CREATE TABLE "users_infos" (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    adresse VARCHAR(255),
-    country VARCHAR(150)
 );
 
 CREATE TABLE "users" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    users_infos UUID,
-    basket_id UUID,
-
-    CONSTRAINT fk_basket_users FOREIGN KEY (basket_id) REFERENCES basket(id),
-    CONSTRAINT fk_users_infos_users FOREIGN KEY (users_infos) REFERENCES users_infos(id) ON DELETE CASCADE
 );
+
+CREATE TABLE "rooms_users" (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    rooms_id UUID,
+    users_id UUID,
+
+    fk_rooms_rooms_users FOREIGN KEY (rooms_id) REFERENCES rooms(id) ON DELETE CASCADE,
+    fk_users_rooms_rooms FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE "chat" (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    content JSONB NOT NULL,
+    users_id UUID,
+
+    fk_users_chat FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
+)
