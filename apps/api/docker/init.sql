02147ad7a0db25e4516cc881401a37f1ff50d8ce
bipblop555 <tbonnet@meylly.com>
2024-08-05 17:40:20 +0200

    modif .sql

--- apps/api/docker/init.sql
+++ apps/api/docker/init.sql
@@ -5,6 +5,7 @@ CREATE DATABASE pictio;
 
 CREATE TABLE "rooms" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    time TIMESTAMP NOT NULL
 );
 
 CREATE TABLE "users" (
@@ -16,7 +17,8 @@ CREATE TABLE "rooms_users" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     rooms_id UUID,
     users_id UUID,
-
+    time TIMESTAMP NOT NULL,
+    
     fk_rooms_rooms_users FOREIGN KEY (rooms_id) REFERENCES rooms(id) ON DELETE CASCADE,
     fk_users_rooms_rooms FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
 );
@@ -25,6 +27,7 @@ CREATE TABLE "chat" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     content JSONB NOT NULL,
     users_id UUID,
+    time TIMESTAMP NOT NULL,
 
     fk_users_chat FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE
 )
